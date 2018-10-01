cf5d374127eca91634997b415886f38bd82b544d
NgoVanHuy <ngovanhuy.cntt2@gmail.com>
2018-10-01 08:18:36 +0700

    bash script commit to github page

---
+++ mkdocs/pushCodeToMaster.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdocs build -d ../docs
+cd ../
+rm -rf /tmp/docs/
+cp -a docs/ /tmp/
+rm -rf docs/
+git checkout master
+cp -a /tmp/docs/ ./
+rm -rf /tmp/docs/
+git add .
+COMMIT_MESSAGE=$1
+if [ -z "$COMMIT_MESSAGE" ];
+then
+	COMMIT_MESSAGE="commit from bash script"
+fi
+git commit -m $1
