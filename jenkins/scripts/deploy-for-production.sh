61b201b54056ce40814d08f14ebfa019fee2dac6
finbert <finbert@trueplus.vn>
2018-10-27 14:44:49 +0700

    add deploy to site 45

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -7,5 +7,5 @@ while [ ! -f ../docs/index.html ]
 do
   sleep 1
 done
-sshpass -p 'Ngovan0241' scp -r helloworld finbert@45.77.42.74:/var/www/html/helloworld
+sshpass -p 'Ngovan0241' scp -r docs finbert@45.77.42.74:/var/www/html/docs
 echo "Deploy done to page: http://45.77.42.74/docs"
